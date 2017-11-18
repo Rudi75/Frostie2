02cb285b437d33fe731a93a56ce17cd3fb71ba81
Rudolf Wagner <wagner.rudi@gmx.at>
2017-11-18 10:10:30 +0100

    https://github.com/Rudi75/Frostie2/issues/6

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public abstract class Trigger : MonoBehaviour
+{
+    [SerializeField] bool stayActive;
+
+    private bool isActive_ = false;
+    private List<TriggerAble> targets = new List<TriggerAble>();
+
+    protected void onChange(bool activate)
+    {
+        if (activate)
+        {
+            isActivated = true;
+        }
+        else if (isActivated && !stayActive)
+        {
+            isActivated = false;
+        }
+    }
+
+
+    public bool isActivated
+    {
+        get
+        {
+            return isActive_;
+        }
+        set
+        {
+            if(isActive_ != value)
+            {
+                isActive_ = value;
+                foreach (TriggerAble target in targets)
+                {
+                    target.trigger();
+                }
+            }
+        }
+    }
+
+    internal void addTarget(TriggerAble triggerAble)
+    {
+        targets.Add(triggerAble);
+    }
+}
+
---
+++ Assets/Scripts/TriggerAble.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public abstract class TriggerAble : MonoBehaviour
+{
+    [SerializeField] List<Trigger> triggers;
+    [SerializeField] bool onlyActiveWhenTriggered;
+
+
+    private bool activated_ = false;
+
+    public void Start()
+    {
+        if (triggers.Count == 0)
+        {
+            Debug.LogError("No Trigger set on TriggerAble" + gameObject.name);
+        }
+        foreach (Trigger trigger in triggers)
+        {
+            trigger.addTarget(this);
+        }
+    }
+
+    abstract protected void performAction();
+
+    public void trigger()
+    {
+        bool allButtonspressed = true;
+        foreach (Trigger trigger in triggers)// all buttons pressed?
+        {
+            if (!trigger.isActivated)
+                allButtonspressed = false;
+        }
+        if (allButtonspressed && !activated_)
+        {
+            performAction();
+            activated_ = true;
+        }
+        else if (onlyActiveWhenTriggered && activated_)
+        {
+            performAction();
+            activated_ = false;
+        }
+    }
+}
+
